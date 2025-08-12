f940a39ba990eeaaf1bf469d249901effe20edd3
Yuvraj Kohli <yuvrajkohli8090ylt@gmail.com>
2025-08-12 14:05:10 +0530

    Add admin breadcrumb-header Blade component

---
+++ resources/views/components/admin/breadcrumb-header.blade.php
@@ -0,0 +1,28 @@
+@props([
+    'icon' => '',
+    'title' => '',
+    'breadcrumbs' => [],
+])
+
+<div class="row mb-4">
+    <div class="col-md-12">
+        <div class="d-flex justify-content-between align-items-center">
+            <h4 class="mb-0">
+                <i class="{{ $icon }} me-2"></i> {!! $title !!}
+            </h4>
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb mb-0">
+                    @foreach ($breadcrumbs as $breadcrumb)
+                        @if (isset($breadcrumb['route']))
+                            <li class="breadcrumb-item">
+                                <a href="{{ route($breadcrumb['route']) }}">{!! $breadcrumb['label'] !!}</a>
+                            </li>
+                        @else
+                            <li class="breadcrumb-item active" aria-current="page">{!! $breadcrumb['label'] !!}</li>
+                        @endif
+                    @endforeach
+                </ol>
+            </nav>
+        </div>
+    </div>
+</div>
