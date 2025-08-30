37bc1deff870925fa90a2c5a40b138f7e636f15e
Yuvraj Kohli <yuvrajkohli8090ylt@gmail.com>
2025-08-30 13:31:35 +0530

    ds

--- app/Models/SafeguardCompliance.php
+++ app/Models/SafeguardCompliance.php
@@ -40,13 +40,8 @@ class SafeguardCompliance extends Model
             ->orderBy('name') // optional ordering
             ->get();
     }
-     public function contractionPhases(): HasMany
+     public function contractionPhases()
     {
-        // Return a "query builder" instead of collection
-        return ContractionPhase::whereIn('id', $this->contraction_phase_ids ?? [])
-            ->orderBy('name') // optional ordering
-            ->getQuery()      // returns query builder instead of collection
-            ->getModel()      // trick to satisfy type hint? Actually, see below
-            ;
+        return ContractionPhase::whereIn('id', $this->contraction_phase_ids ?? [])->get();
     }
 }
